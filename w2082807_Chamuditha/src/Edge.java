014f61995cb8d861ba256e5af3142dababdd06d5
chamudithaweeramanthri <chamudithaweeramanthri@gmail.com>
2025-04-23 20:18:48 +0530

    Update Edge.java

--- w2082807_Chamuditha/src/Edge.java
+++ w2082807_Chamuditha/src/Edge.java
@@ -5,7 +5,31 @@ public class Edge {
     int capacity;
     int flow;
 
+    // connect edge with reverse edge
+    Edge residual;
 
+    //parameterized constructor
+    public Edge(int from ,int to  ,int capacity,int flow){
+        this.from = from;
+        this.to = to;
+        this.capacity = capacity;
+        this.flow = 0;
 
+    }
+
+    //calculate the remining space from full capacity in edge
+    public int remainingCapacity(){
+        return capacity - flow;
+    }
+
+    public void augment(int bottleNeck){
+        flow += bottleNeck;
+        residual.flow -= bottleNeck;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Edge %d -> %d | flow = %d / %d",from,to,flow,capacity);
+    }
 
 }
