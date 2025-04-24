1fdf8a7101dc14abe9228e6772d9879cca4d17e5
chamudithaweeramanthri <chamudithaweeramanthri@gmail.com>
2025-04-24 19:44:09 +0530

    Update Main.java

--- w2082807_Chamuditha/src/Main.java
+++ w2082807_Chamuditha/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
-
+        try {
+            String filePath = "network.txt"; // Make sure this file is in your working directory
+            Graph graph = Parser.parseFromFile(filePath);
+            MaxFlowSolver solver = new MaxFlowSolver();
+            int maxFlow = solver.getMaxFlow(graph, 0, graph.n - 1);
+            System.out.println("Maximum Flow: " + maxFlow);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
