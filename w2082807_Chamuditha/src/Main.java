921760052f926cf94374c9fe1eaccef3e4fba9fd
chamudithaweeramanthri <chamudithaweeramanthri@gmail.com>
2025-04-26 18:30:59 +0530

    class files updated

--- w2082807_Chamuditha/src/Main.java
+++ w2082807_Chamuditha/src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            String filePath = "network.txt"; // Make sure this file is in your working directory
+            String filePath = "F:\\Algorithmic-analysis-of-network-flow\\ladder_1.txt"; // Make sure this file is in your working directory
             Graph graph = Text.parseFromFile(filePath);
             MaxFlowSolver solver = new MaxFlowSolver();
+            long startTime = System.currentTimeMillis(); // Start time
             int maxFlow = solver.getMaxFlow(graph, 0, graph.nodes - 1);
+            long endTime = System.currentTimeMillis(); // End time
             System.out.println("Maximum Flow: " + maxFlow);
+            System.out.println("Times goes to complete : "+(endTime - startTime)+" Seconds.");
         } catch (Exception e) {
             e.printStackTrace();
         }
